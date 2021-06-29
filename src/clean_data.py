210a722de83db2199ba07bb13f67489224ec2db7
Yizhen Liu <imliuyzh@outlook.com>
2021-06-28 17:28:21 -0700

    Add new data and revise clean_data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -5,12 +5,18 @@ from html import unescape
 from random import uniform
 from time import asctime, sleep
 
+SPREADSHEET_FILE = "2017-2018.xlsx" # Change to the name of the file under the "temp" folder you want to parse
 TERM_DICT = {"FALL": "92", "WINTER": "03", "SPRING": "14"}
-SPREADSHEET_FILE = "2013-2014.xlsx"
 
 def _get_data(request: urllib.request.Request, course_code: str) -> dict:
+    '''Send a request to https://www.reg.uci.edu/perl/WebSoc?
+    Submit=Display+XML+Results&YearTerm=???&Breadth=ANY&Dept=+ALL&Division=ANY&
+    ClassType=ALL&FullCourses=ANY&CancelledCourses=Exclude&CourseCodes=???
+    in order to parse the XML to get the correct data.'''
+    
     info = {
         "success": False,
+        "error_message": None,
         "dept_name": None,
         "course_code": course_code,
         "course_number": None,
@@ -33,14 +39,17 @@ def _get_data(request: urllib.request.Request, course_code: str) -> dict:
                 for prof_element in prof_list:
                     if prof_element != "\n" and prof_element.string.strip() != "STAFF":
                         info["instructors"].append(prof_element.string.strip())
+            else:
+                info["error_message"] = "Failed to find the corresponding course on WebSOC."
     except urllib.error.HTTPError as error_object:
-        print(f"[{asctime()}] Server returns error code ({error_object.code}).")
+        info["error_message"] = f"Server returns error code ({error_object.code})."
     except urllib.error.URLError as error_object:
-        print(f"[{asctime()}] A network error occurs ({error_object.reason}).")
+        info["error_message"] = error_object.reason
 
     return info
 
 def _build_request(course_info: dict) -> urllib.request.Request:
+    '''Build an object to send a correct query to the WebSOC service.'''
     parameters = bytes("Submit=Display+XML+Results&"
                        + f"YearTerm={course_info['quarter']}&Breadth=ANY&"
                        + "Dept=+ALL&Division=ANY&ClassType=ALL&"
@@ -53,49 +62,54 @@ def _build_request(course_info: dict) -> urllib.request.Request:
     return request
 
 def _jump_to_first_not_processed_row(sheet: openpyxl.worksheet.worksheet.Worksheet) -> int or None:
+    '''Find the first row in the spreadsheet that is not processed (has 'F' in the 'Processed' row).'''
     for num in range(2, sheet.max_row + 1):
         if sheet[f"P{num}"].value == "F":
             return num
     return None
 
-def _update_spreadsheet(index: int, info: dict, sheet: openpyxl.worksheet.worksheet.Worksheet) -> None:
-    sheet["C" + str(index)].value = info["dept_name"]
-    sheet["D" + str(index)].value = info["course_number"]
-    sheet["E" + str(index)].value = info["course_code"]
-    sheet["F" + str(index)].value = info["course_title"]
-    sheet["G" + str(index)].value = "; ".join(info["instructors"])
-    sheet["P" + str(index)].value = "T"
+def _update_spreadsheet(row: int, info: dict, sheet: openpyxl.worksheet.worksheet.Worksheet) -> None:
+    '''Change the information on the spreadsheet to the one listed on WebSOC.'''
+    sheet["C" + str(row)].value = info["dept_name"]
+    sheet["D" + str(row)].value = info["course_number"]
+    sheet["E" + str(row)].value = info["course_code"]
+    sheet["F" + str(row)].value = info["course_title"]
+    sheet["G" + str(row)].value = "; ".join(info["instructors"])
+    sheet["P" + str(row)].value = "T"
 
 def clean_data() -> None:
+    '''Start to parse the data from WebSOC.'''
     try:
         file = openpyxl.load_workbook("../temp/" + SPREADSHEET_FILE, data_only=True)
-        for sheetname in file.sheetnames:
-            sheet = file[sheetname]
-            start = _jump_to_first_not_processed_row(sheet)
-            
-            if start is not None:
-                while start <= sheet.max_row:
-                    course_code = str(sheet["E" + str(start)].value) if len(str(sheet["E" + str(start)].value)) == 5 else "0" + str(sheet["E" + str(start)].value)
-                    print(f"[{asctime()}] Processing course #{course_code}.")
+        with open("log.txt", "w") as error_log:
+            for sheetname in file.sheetnames:
+                sheet = file[sheetname]
+                start = _jump_to_first_not_processed_row(sheet)
+                
+                if start is not None:
+                    while start <= sheet.max_row:
+                        course_code = str(sheet["E" + str(start)].value) if len(str(sheet["E" + str(start)].value)) == 5 else "0" + str(sheet["E" + str(start)].value)
+                        print(f"[{asctime()}] Processing course #{course_code}.")
 
-                    request = _build_request({
-                        "quarter": sheetname.split()[1] + "-" + TERM_DICT[sheetname.split()[0].upper()],
-                        "course_code": course_code
-                    })
-                    info = _get_data(request, course_code)
+                        request = _build_request({
+                            "quarter": sheetname.split()[1] + "-" + TERM_DICT[sheetname.split()[0].upper()],
+                            "course_code": course_code
+                        })
+                        info = _get_data(request, course_code)
 
-                    if info["success"] == True:
-                        _update_spreadsheet(start, info, sheet)
-                        file.save("../processed_data/_" + SPREADSHEET_FILE)
-                    else:
-                        print(f"[{asctime()}] Failed to process course #{course_code}.")
-                    start += 1
+                        if info["success"] == True:
+                            _update_spreadsheet(start, info, sheet)
+                            file.save("../processed_data/_" + SPREADSHEET_FILE)
+                        else:
+                            print(f"[{asctime()}] Failed to process course #{course_code}: " + info["error_message"])
+                            error_log.write(f"[{asctime()}] Failed to process course #{course_code}: {info["error_message"]}\n")
+                        start += 1
 
-                    pause_time = uniform(5, 9)
-                    print(f"[{asctime()}] Halt the process for {pause_time:.2f} seconds to protect the WebSOC server.")
-                    sleep(pause_time)
+                        pause_time = uniform(5, 9)
+                        print(f"[{asctime()}] Halt the process for {pause_time:.2f} seconds to protect the WebSOC server.")
+                        sleep(pause_time)
     except KeyboardInterrupt:
-        print(f"[{asctime()}] Exiting the program...")
+        print(f"[{asctime()}] Exiting the program...")]
 
 if __name__ == "__main__":
     clean_data()
