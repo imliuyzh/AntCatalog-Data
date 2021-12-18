0e50cfc604be5f2d0196ad8ee8464339cdc53c4e
Yizhen Liu <imliuyzh@outlook.com>
2021-12-18 15:14:41 -0800

    Modify database design and add ZotCurve data

--- src/create_db.py
+++ src/create_db.py
@@ -3,119 +3,128 @@ import openpyxl
 from pathlib import Path
 from time import asctime
 
-def _create_index() -> None:
-    '''Index several fields on the Course and Instructor table.'''
-    connection = sqlite3.connect("../data.db")
-    try:
-        print(f"[{asctime()}] Begin creating index")
-        connection.executescript(
-            """
-            CREATE INDEX CourseCodeTermIndex ON Course (course_code, term);
-            CREATE INDEX CourseNumberDepartmentTermIndex ON Course (course_number, department, term);
-            CREATE INDEX InstructorNameIndex ON Instructor (name);
-            """
-        )
-        print(f"[{asctime()}] Finished creating index")
-        connection.commit()
-    except sqlite3.Error as error:
-        print(f"[{asctime()}] Failed to create index: {error}")
-        connection.rollback()
-    finally:
-        if connection is not None:
-            connection.close()
+class DBCreator:
+    def __init__(self):
+        self._count = 0
 
-def _load_data_per_year(workbook: openpyxl.workbook.workbook.Workbook) -> ([(str, int, str, str, str, int, int, int, int, int, int, int, float)], [(str, int, str)]):
-    '''Return data in an academic year.'''
-    all_courses, all_instructors = [], []
-    for quarter in workbook.sheetnames:
-        print(f"[{asctime()}] Processing data from {quarter}")
-        sheet = workbook[quarter]
-        
-        for row in range(2, sheet.max_row + 1):
-            department = str(sheet["C" + str(row)].value)
-            course_number = str(sheet["D" + str(row)].value)
-            course_code = int(sheet["E" + str(row)].value)
-            course_title = str(sheet["F" + str(row)].value)
-            grade_a_count = int(sheet["H" + str(row)].value)
-            grade_b_count = int(sheet["I" + str(row)].value)
-            grade_c_count = int(sheet["J" + str(row)].value)
-            grade_d_count = int(sheet["K" + str(row)].value)
-            grade_f_count = int(sheet["L" + str(row)].value)
-            grade_p_count = int(sheet["M" + str(row)].value)
-            grade_np_count = int(sheet["N" + str(row)].value)
-            average_gpa = float(sheet["O" + str(row)].value) if sheet["O" + str(row)].value is not None else 0
-            instructors = sheet["G" + str(row)].value.split("; ")
-            
-            all_courses.append((quarter, course_code, department.upper(), course_number.upper(), course_title.upper(), grade_a_count, grade_b_count, grade_c_count, grade_d_count, grade_f_count, grade_p_count, grade_np_count, average_gpa))
-            for instructor in instructors:
-                all_instructors.append((quarter, course_code, instructor.upper()))
+    def run(self) -> None:
+        self._create_table()
+        self._insert_data()
+        self._create_index()
 
-        print(f"[{asctime()}] Finished processing data from {quarter}")
-    return all_courses, all_instructors
+    def _create_table(self) -> None:
+        '''Create a file named data.db and create two SQL tables.'''
+        connection = sqlite3.connect("../data.db")
+        try:
+            print(f"[{asctime()}] Begin creating table")
+            connection.executescript(
+                """
+                CREATE TABLE Course (
+                    course_id INTEGER NOT NULL,
+                    term TEXT NOT NULL,
+                    course_code INTEGER NOT NULL,
+                    department TEXT NOT NULL,
+                    course_number TEXT NOT NULL,
+                    course_title TEXT NOT NULL,
+                    grade_a_count INTEGER NOT NULL,
+                    grade_b_count INTEGER NOT NULL,
+                    grade_c_count INTEGER NOT NULL,
+                    grade_d_count INTEGER NOT NULL,
+                    grade_f_count INTEGER NOT NULL,
+                    grade_p_count INTEGER NOT NULL,
+                    grade_np_count INTEGER NOT NULL,
+                    gpa_avg REAL NOT NULL,
+                    CONSTRAINT CoursePrimaryKey PRIMARY KEY (course_id)
+                );
 
-def _insert_data() -> None:
-    '''Loop through the spreadsheets to parse and store all the course statistics into data.db.'''
-    folder = Path(r"../processed_data")
-    connection = sqlite3.connect("../data.db")
-    
-    try:
-        for spreadsheet_path in folder.iterdir():
-            print(f"[{asctime()}] Working with the file {spreadsheet_path}")
-            courses, instructors = _load_data_per_year(openpyxl.load_workbook(str(spreadsheet_path), data_only=True))
-            connection.executemany("INSERT INTO Course VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", courses)
-            connection.executemany("INSERT INTO Instructor VALUES (?, ?, ?);", instructors)
+                CREATE TABLE Instructor (
+                    course_id INTEGER NOT NULL,
+                    name TEXT NOT NULL,
+                    CONSTRAINT InstructorPrimaryKey PRIMARY KEY (course_id, name),
+                    CONSTRAINT InstructorCourseCodeForeignKey FOREIGN KEY (course_id) REFERENCES Course(course_id) ON DELETE CASCADE
+                );
+                """
+            )
+            print(f"[{asctime()}] Finished creating table")
             connection.commit()
-    except sqlite3.Error as error:
-        print(f"[{asctime()}] Failed to insert data: {error}")
-        connection.rollback()
-    finally:
-        if connection is not None:
-            connection.close()
+        except sqlite3.Error as error:
+            print(f"[{asctime()}] Failed to create table: {error}")
+            connection.rollback()
+        finally:
+            if connection is not None:
+                connection.close()
 
-def _create_table() -> None:
-    '''Create a file named data.db and create two SQL tables.'''
-    connection = sqlite3.connect("../data.db")
-    try:
-        print(f"[{asctime()}] Begin creating table")
-        connection.executescript(
-            """
-            CREATE TABLE Course (
-                term TEXT,
-                course_code INTEGER,
-                department TEXT NOT NULL,
-                course_number TEXT NOT NULL,
-                course_title TEXT NOT NULL,
-                grade_a_count INTEGER NOT NULL,
-                grade_b_count INTEGER NOT NULL,
-                grade_c_count INTEGER NOT NULL,
-                grade_d_count INTEGER NOT NULL,
-                grade_f_count INTEGER NOT NULL,
-                grade_p_count INTEGER NOT NULL,
-                grade_np_count INTEGER NOT NULL,
-                gpa_avg REAL NOT NULL,
-                CONSTRAINT CoursePrimaryKey PRIMARY KEY (term, course_code)
-            );
+    def _load_data_per_year(self, workbook: openpyxl.workbook.workbook.Workbook) -> ([(int, str, int, str, str, str, int, int, int, int, int, int, int, float)], [(int, str)]):
+        '''Return data in an academic year.'''
+        all_courses, all_instructors = [], []
+        for quarter in workbook.sheetnames:
+            print(f"[{asctime()}] Processing data from {quarter}")
+            sheet = workbook[quarter]
+            
+            for row in range(2, sheet.max_row + 1):
+                department = str(sheet["C" + str(row)].value)
+                course_number = str(sheet["D" + str(row)].value)
+                course_code = int(sheet["E" + str(row)].value)
+                course_title = str(sheet["F" + str(row)].value)
+                grade_a_count = int(sheet["H" + str(row)].value)
+                grade_b_count = int(sheet["I" + str(row)].value)
+                grade_c_count = int(sheet["J" + str(row)].value)
+                grade_d_count = int(sheet["K" + str(row)].value)
+                grade_f_count = int(sheet["L" + str(row)].value)
+                grade_p_count = int(sheet["M" + str(row)].value)
+                grade_np_count = int(sheet["N" + str(row)].value)
+                average_gpa = float(sheet["O" + str(row)].value) if sheet["O" + str(row)].value is not None else 0
+                instructors = sheet["G" + str(row)].value.split("; ")
+                
+                all_courses.append((self._count, quarter, course_code, department.upper(), course_number.upper(), course_title.upper(), grade_a_count, grade_b_count, grade_c_count, grade_d_count, grade_f_count, grade_p_count, grade_np_count, average_gpa))
+                for instructor in instructors:
+                    all_instructors.append((self._count, instructor.upper()))
+                
+                self._count += 1
 
-            CREATE TABLE Instructor (
-                term TEXT,
-                course_code INTEGER,
-                name TEXT,
-                CONSTRAINT InstructorPrimaryKey PRIMARY KEY (term, course_code, name),
-                CONSTRAINT InstructorTermForeignKey FOREIGN KEY (term) REFERENCES Course(term),
-                CONSTRAINT InstructorCourseCodeForeignKey FOREIGN KEY (course_code) REFERENCES Course(course_code) ON DELETE CASCADE
-            );
-            """
-        )
-        print(f"[{asctime()}] Finished creating table")
-        connection.commit()
-    except sqlite3.Error as error:
-        print(f"[{asctime()}] Failed to create table: {error}")
-        connection.rollback()
-    finally:
-        if connection is not None:
-            connection.close()
+            print(f"[{asctime()}] Finished processing data from {quarter}")
+        return all_courses, all_instructors
+
+    def _insert_data(self) -> None:
+        '''Loop through the spreadsheets to parse and store all the course statistics into data.db.'''
+        folder = Path(r"../processed_data")
+        connection = sqlite3.connect("../data.db")
+
+        try:
+            for spreadsheet_path in folder.iterdir():
+                print(f"[{asctime()}] Working with the file {spreadsheet_path}")
+                courses, instructors = self._load_data_per_year(openpyxl.load_workbook(str(spreadsheet_path), data_only=True))
+                connection.executemany("INSERT INTO Course VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", courses)
+                connection.executemany("INSERT INTO Instructor VALUES (?, ?);", instructors)
+                connection.commit()
+        except sqlite3.Error as error:
+            print(f"[{asctime()}] Failed to insert data: {error}")
+            connection.rollback()
+        finally:
+            if connection is not None:
+                connection.close()
+
+    def _create_index(self) -> None:
+        '''Index several fields on the Course and Instructor table.'''
+        connection = sqlite3.connect("../data.db")
+        try:
+            print(f"[{asctime()}] Begin creating index")
+            connection.executescript(
+                """
+                CREATE INDEX CourseCodeTermIndex ON Course (course_code, term);
+                CREATE INDEX CourseNumberDepartmentTermIndex ON Course (course_number, department, term);
+                CREATE INDEX InstructorNameIndex ON Instructor (name);
+                """
+            )
+            print(f"[{asctime()}] Finished creating index")
+            connection.commit()
+        except sqlite3.Error as error:
+            print(f"[{asctime()}] Failed to create index: {error}")
+            connection.rollback()
+        finally:
+            if connection is not None:
+                connection.close()
 
 if __name__ == "__main__":
-    _create_table()
-    _insert_data()
-    _create_index()
+    db_creator = DBCreator()
+    db_creator.run()
